e56e0cc3fb99f2f9d4735e150aa3369f891a2842
freetimezp <kraftukrnet@ukr.net>
2023-04-24 23:49:25 +0300

    03. js-cars-02. Navbar component.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,40 @@
 import './Navbar.css';
 
+import Logo from '../../Assets/images/logo.png';
+
+import { IoIosCloseCircle } from 'react-icons/io';
+import { TbGridDots } from 'react-icons/tb';
+
 const Navbar = () => {
     return (
-        <div>
+        <div className='header'>
+            <div className="logoDiv">
+                <img src={Logo} alt="logo" className='logo' />
+            </div>
 
+            <div className="navbar">
+                <ul className="menu">
+                    <li className="listItem">
+                        <a href="/" className="link">Used Cars</a>
+                    </li>
+                    <li className="listItem">
+                        <a href="/" className="link">New Cars</a>
+                    </li>
+                    <li className="listItem">
+                        <a href="/" className="link">Auction</a>
+                    </li>
+                    <li className="listItem">
+                        <a href="/" className="link">Sell</a>
+                    </li>
+                </ul>
+
+                <IoIosCloseCircle className='icon closeIcon' />
+            </div>
+
+            <div className="signUp flex">
+                <div className="text">Sign Up</div>
+                <TbGridDots className='icon toggleNavbarIcon' />
+            </div>
         </div>
     );
 }
