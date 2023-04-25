6e901b93bf74487fe30fccb3c92fe0bb7ebbf3cc
freetimezp <kraftukrnet@ukr.net>
2023-04-25 20:38:32 +0300

    05. js-cars-02. Show, hide menu.

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
+
 import './Navbar.css';
 
 import Logo from '../../Assets/images/logo.png';
@@ -6,34 +9,44 @@ import { IoIosCloseCircle } from 'react-icons/io';
 import { TbGridDots } from 'react-icons/tb';
 
 const Navbar = () => {
+    const [navbar, setNavbar] = useState('navbar');
+
+    const showNavbar = () => {
+        setNavbar('navbar showNavbar');
+    };
+
+    const removeNavbar = () => {
+        setNavbar('navbar');
+    };
+
     return (
         <div className='header'>
             <div className="logoDiv">
                 <img src={Logo} alt="logo" className='logo' />
             </div>
 
-            <div className="navbar">
+            <div className={navbar}>
                 <ul className="menu">
-                    <li className="listItem">
+                    <li className="listItem" onClick={removeNavbar}>
                         <a href="/" className="link">Used Cars</a>
                     </li>
-                    <li className="listItem">
+                    <li className="listItem" onClick={removeNavbar}>
                         <a href="/" className="link">New Cars</a>
                     </li>
-                    <li className="listItem">
+                    <li className="listItem" onClick={removeNavbar}>
                         <a href="/" className="link">Auction</a>
                     </li>
-                    <li className="listItem">
+                    <li className="listItem" onClick={removeNavbar}>
                         <a href="/" className="link">Sell</a>
                     </li>
                 </ul>
 
-                <IoIosCloseCircle className='icon closeIcon' />
+                <IoIosCloseCircle className='icon closeIcon' onClick={removeNavbar} />
             </div>
 
             <div className="signUp flex">
                 <div className="text">Sign Up</div>
-                <TbGridDots className='icon toggleNavbarIcon' />
+                <TbGridDots className='icon toggleNavbarIcon' onClick={showNavbar} />
             </div>
         </div>
     );
