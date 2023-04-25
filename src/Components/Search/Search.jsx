4d9352c92fcfe30c666266eb51c9030e93628514
freetimezp <kraftukrnet@ukr.net>
2023-04-25 21:06:52 +0300

    06. js-cars-02. Search component.

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,9 +1,23 @@
 import './Search.css';
 
+import { AiOutlineSearch } from 'react-icons/ai';
+
 const Search = () => {
     return (
-        <div>
-
+        <div className='search'>
+            <div className="secContainer container">
+                <h3 className="title">Which vehicle you are looking for?</h3>
+                <div className="searchDiv grid">
+                    <input type="text" placeholder="Type" />
+                    <input type="number" placeholder="Year" />
+                    <input type="text" placeholder="Model" />
+                    <input type="number" placeholder="Price" />
+                    <button className='btn primaryBtn flex'>
+                        <AiOutlineSearch className='icon' />
+                        <span>Search</span>
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
