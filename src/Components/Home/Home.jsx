af12ff0da81082bc4c3aa23b609263f00b33231a
freetimezp <kraftukrnet@ukr.net>
2023-04-25 20:02:49 +0300

    04. js-cars-02. Home component.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,26 @@
 import './Home.css';
 
+import homeImg from '../../Assets/images/home.png';
+
 const Home = () => {
     return (
-        <div>
+        <div className='home'>
+            <div className="secContainer">
+                <div className="homeText">
+                    <span className="homeSpan">Meet your new car</span>
+                    <h1 className="homeTitle">
+                        Honda Civic Type R
+                    </h1>
+                    <div className="btns flex">
+                        <button className="btn">More Details</button>
+                        <button className="btn primaryBtn">Test Drive</button>
+                    </div>
+                </div>
 
+                <div className="homeImage">
+                    <img src={homeImg} alt="home" />
+                </div>
+            </div>
         </div>
     );
 }
